0235523b5b7e5e688e29478f6a4c11376c71bb63
Aldyernesto <aldyernesto@gmail.com>
2025-12-17 01:37:08 +0700

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -35,6 +35,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/categories/{id}/products', [CategoryController::class, 'show']);
